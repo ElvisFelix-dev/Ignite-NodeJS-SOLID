cec17c0ae8ebfb52d73070094b821d554ba4cd8f
Elvis Felix <elvisjaspion@gmal.com>
2022-01-13 17:58:49 -0300

    Finalizando o desafio

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, Request, Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
